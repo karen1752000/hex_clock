0ce054c942f96111bb2d03b857499040edb5243d
Karen Mitchell <karen1752000@yahoo.com>
2018-01-20 17:08:43 -0500

    Added hex color string ro javascript

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-var time = new Date();
-console.log(time.getHours() % 12);
-console.log(time.getMinutes());
-console.log(time.getSeconds());
-
 var clock = document.getElementById('clock');
+var hexColor = document.getElementById('hex-color');
+
+function hexClock() {
+	var time =newDate();
+	var hours = time.gethours().toString();
+	var minutes = time.getMinutes()toString();
+	var seconds = time.getseconds().toString();
+
+	if (hours.length < 2) {
+		hours = '0' + hours;
+	}
+
+	if (minutes.length < 2) {
+		minutes = '0' + minutes;
+	}
+
+	if (seconds.length < 2) {
+		seconds = '0' + seconds;
+	}
+
+	var hexColorStr = '#' + hours + minutes + seconds;
+}
